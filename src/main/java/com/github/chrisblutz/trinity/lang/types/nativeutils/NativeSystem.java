2df9f0ad0c02f7f1a119a339f16d1d0aa1c24d7b
Christopher Lutz <lutzblox@gmail.com>
2017-06-14 22:11:23 -0400

    Add native system property management, add 'trinity.home' system property

--- src/main/java/com/github/chrisblutz/trinity/lang/types/nativeutils/NativeSystem.java
+++ src/main/java/com/github/chrisblutz/trinity/lang/types/nativeutils/NativeSystem.java
@@ -1,7 +1,5 @@
 package com.github.chrisblutz.trinity.lang.types.nativeutils;
 
-import com.github.chrisblutz.trinity.info.TrinityInfo;
-import com.github.chrisblutz.trinity.lang.ClassRegistry;
 import com.github.chrisblutz.trinity.lang.TYObject;
 import com.github.chrisblutz.trinity.lang.procedures.ProcedureAction;
 import com.github.chrisblutz.trinity.lang.types.maps.TYMap;
@@ -9,6 +7,7 @@ import com.github.chrisblutz.trinity.lang.types.numeric.TYLong;
 import com.github.chrisblutz.trinity.lang.types.strings.TYString;
 import com.github.chrisblutz.trinity.natives.NativeStorage;
 import com.github.chrisblutz.trinity.natives.TrinityNatives;
+import com.github.chrisblutz.trinity.natives.TrinityProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,18 +39,7 @@ class NativeSystem {
         });
         TrinityNatives.registerMethod("Trinity.System", "loadProperties", true, null, null, null, (runtime, thisObj, params) -> {
             
-            Map<TYObject, TYObject> map = new HashMap<>();
-            
-            map.put(new TYString("trinity.name"), TrinityNatives.getObjectFor(TrinityInfo.get("trinity.name")));
-            map.put(new TYString("trinity.version"), TrinityNatives.getObjectFor(TrinityInfo.get("trinity.version")));
-            map.put(new TYString("os.arch"), TrinityNatives.getObjectFor(System.getProperty("os.arch")));
-            map.put(new TYString("os.name"), TrinityNatives.getObjectFor(System.getProperty("os.name")));
-            map.put(new TYString("os.version"), TrinityNatives.getObjectFor(System.getProperty("os.version")));
-            map.put(new TYString("user.dir"), TrinityNatives.getObjectFor(System.getProperty("user.dir")));
-            map.put(new TYString("user.home"), TrinityNatives.getObjectFor(System.getProperty("user.home")));
-            map.put(new TYString("user.name"), TrinityNatives.getObjectFor(System.getProperty("user.name")));
-            
-            ClassRegistry.getClass("Trinity.System").setVariable("properties", new TYMap(map));
+            TrinityProperties.load();
             
             return TYObject.NONE;
         });
---
+++ src/main/java/com/github/chrisblutz/trinity/natives/TrinityProperties.java
@@ -0,0 +1,95 @@
+package com.github.chrisblutz.trinity.natives;
+
+import com.github.chrisblutz.trinity.info.TrinityInfo;
+import com.github.chrisblutz.trinity.lang.ClassRegistry;
+import com.github.chrisblutz.trinity.lang.TYObject;
+import com.github.chrisblutz.trinity.lang.errors.Errors;
+import com.github.chrisblutz.trinity.lang.types.maps.TYMap;
+import com.github.chrisblutz.trinity.lang.types.strings.TYString;
+import com.github.chrisblutz.trinity.utils.FileUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * @author Christopher Lutz
+ */
+public class TrinityProperties {
+    
+    private static Map<TYObject, TYObject> map = new HashMap<>();
+    
+    /**
+     * Sets the Trinity system property with the specified name to the specified value.
+     * <p>
+     * This is the native Java equivalent of the following Trinity code:
+     * <pre>
+     *     System.setProperty('property', 'value')
+     * </pre>
+     *
+     * @param property The name of the property
+     * @param value    The {@code TYObject} representing the value of the property.  This may
+     *                 be retrieved using {@code TrinityNatives.getObjectFor(obj)}
+     */
+    public static void setProperty(String property, TYObject value) {
+        
+        map.put(new TYString(property), value);
+    }
+    
+    /**
+     * Retrieves the Trinity system property with the specified name.
+     * <p>
+     * This is the native Java equivalent of the following Trinity code:
+     * <pre>
+     *     System.getProperty('property')
+     * </pre>
+     *
+     * @param property The name of the property
+     * @return The {@code TYObject} representing the value of the property, or {@code TYObject.NIL} if
+     * the property does not exist.  (<strong>NOTE:</strong> This method may return {@code TYObject.NIL} even if a property
+     * exists, if that property is set to {@code nil})
+     */
+    public static TYObject getProperty(String property) {
+        
+        return map.getOrDefault(property, TYObject.NIL);
+    }
+    
+    /**
+     * Loads Trinity's default system properties into the Trinity {@code System} class's {@code @@properties} variable.
+     * These include the following:
+     * <pre>
+     *     trinity.home
+     *     trinity.name
+     *     trinity.version
+     *     os.arch
+     *     os.name
+     *     os.version
+     *     user.dir
+     *     user.home
+     *     user.name
+     * </pre>
+     */
+    public static void load() {
+        
+        setProperty("trinity.name", TrinityNatives.getObjectFor(TrinityInfo.get("trinity.name")));
+        setProperty("trinity.version", TrinityNatives.getObjectFor(TrinityInfo.get("trinity.version")));
+        setProperty("os.arch", TrinityNatives.getObjectFor(System.getProperty("os.arch")));
+        setProperty("os.name", TrinityNatives.getObjectFor(System.getProperty("os.name")));
+        setProperty("os.version", TrinityNatives.getObjectFor(System.getProperty("os.version")));
+        setProperty("user.dir", TrinityNatives.getObjectFor(System.getProperty("user.dir")));
+        setProperty("user.home", TrinityNatives.getObjectFor(System.getProperty("user.home")));
+        setProperty("user.name", TrinityNatives.getObjectFor(System.getProperty("user.name")));
+        
+        try {
+            
+            setProperty("trinity.home", TrinityNatives.getObjectFor(FileUtils.getTrinityHome().getCanonicalPath()));
+            
+        } catch (IOException e) {
+            
+            Errors.throwError("Trinity.Errors.IOError", "Unable to determine Trinity's home directory.");
+        }
+        
+        ClassRegistry.getClass("Trinity.System").setVariable("properties", new TYMap(map));
+    }
+}
