26c37f266203c108c76986682a89f03a977143ab
Christopher Lutz <lutzblox@gmail.com>
2017-06-16 20:33:07 -0400

    Convert NativeMethod to use new overflow parameter functionality

--- src/main/java/com/github/chrisblutz/trinity/lang/types/nativeutils/NativeMethod.java
+++ src/main/java/com/github/chrisblutz/trinity/lang/types/nativeutils/NativeMethod.java
@@ -3,10 +3,12 @@ package com.github.chrisblutz.trinity.lang.types.nativeutils;
 import com.github.chrisblutz.trinity.lang.TYMethod;
 import com.github.chrisblutz.trinity.lang.TYObject;
 import com.github.chrisblutz.trinity.lang.types.TYMethodObject;
+import com.github.chrisblutz.trinity.lang.types.arrays.TYArray;
 import com.github.chrisblutz.trinity.natives.NativeStorage;
 import com.github.chrisblutz.trinity.natives.TrinityNatives;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,14 +21,21 @@ class NativeMethod {
         TrinityNatives.registerMethod("Trinity.Method", "getRequiredArguments", false, null, null, null, null, (runtime, thisObj, params) -> NativeStorage.getMandatoryArguments(TrinityNatives.cast(TYMethodObject.class, thisObj).getInternalMethod()));
         TrinityNatives.registerMethod("Trinity.Method", "getOptionalArguments", false, null, null, null, null, (runtime, thisObj, params) -> NativeStorage.getOptionalArguments(TrinityNatives.cast(TYMethodObject.class, thisObj).getInternalMethod()));
         TrinityNatives.registerMethod("Trinity.Method", "getBlockArgument", false, null, null, null, null, (runtime, thisObj, params) -> NativeStorage.getBlockArgument(TrinityNatives.cast(TYMethodObject.class, thisObj).getInternalMethod()));
-        TrinityNatives.registerMethod("Trinity.Method", "invoke", false, new String[]{"obj"}, null, null, null, (runtime, thisObj, params) -> {
+        TrinityNatives.registerMethod("Trinity.Method", "invoke", false, new String[]{"obj"}, null, null, "args", (runtime, thisObj, params) -> {
             
             TYObject invokeThis = runtime.getVariable("obj");
-            TYObject[] newParams = Arrays.copyOfRange(params, 1, params.length);
+            
+            TYObject args = runtime.getVariable("args");
+            List<TYObject> methodParams = new ArrayList<>();
+            if (args instanceof TYArray) {
+                
+                methodParams.addAll(((TYArray) args).getInternalList());
+            }
+            
             TYMethod method = TrinityNatives.cast(TYMethodObject.class, thisObj).getInternalMethod();
             String name = method.getName();
             
-            return method.getContainerClass().tyInvoke(name, runtime, null, null, invokeThis, newParams);
+            return method.getContainerClass().tyInvoke(name, runtime, null, null, invokeThis, methodParams.toArray(new TYObject[methodParams.size()]));
         });
         TrinityNatives.registerMethod("Trinity.Method", "getName", false, null, null, null, null, (runtime, thisObj, params) -> NativeStorage.getMethodName(TrinityNatives.cast(TYMethodObject.class, thisObj).getInternalMethod()));
         TrinityNatives.registerMethod("Trinity.Method", "isStatic", false, null, null, null, null, (runtime, thisObj, params) -> NativeStorage.isMethodStatic(TrinityNatives.cast(TYMethodObject.class, thisObj).getInternalMethod()));
