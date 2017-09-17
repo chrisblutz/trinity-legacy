4d69c2764b5b9967004cada92ad81918fa5fe82c
Christopher Lutz <lutzblox@gmail.com>
2017-09-17 13:39:52 -0400

    Use message in library compilation errors

--- src/main/java/com/github/chrisblutz/trinity/libraries/compiling/CompileException.java
+++ src/main/java/com/github/chrisblutz/trinity/libraries/compiling/CompileException.java
@@ -7,7 +7,7 @@ public class CompileException extends RuntimeException {
     
     public CompileException(String file, long line, String message) {
         
-        super("Compilation error on line " + line + " in " + file + ".");
+        super("Compilation error on line " + line + " in " + file + ": " + message);
         setStackTrace(new StackTraceElement[]{});
     }
 }
